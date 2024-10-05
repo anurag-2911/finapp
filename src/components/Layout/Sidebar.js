786952069074f6e8359f548f58741700b9ba3413
Anurag Kumar <AnKumar@flexera.com>
2024-10-05 18:29:33 +0530

    context

--- src/components/Layout/Sidebar.js
+++ src/components/Layout/Sidebar.js
@@ -26,19 +26,19 @@ const Sidebar = () => {
       }}
     >
       <List>
-        <ListItem button component={Link} to="/dashboard" disabled={!isUserTokenValid}>
+        <ListItem button component={Link} to="/dashboard" disabled={!(isUserTokenValid==="")}>
           <ListItemIcon sx={{ color: 'white' }}>
             <Dashboard />
           </ListItemIcon>
           <ListItemText primary="Dashboard" />
         </ListItem>
-        <ListItem button component={Link} to="/apply-finance" disabled={!isUserTokenValid}>
+        <ListItem button component={Link} to="/apply-finance" disabled={!(isUserTokenValid==="")}>
           <ListItemIcon sx={{ color: 'white' }}>
             <MonetizationOn />
           </ListItemIcon>
           <ListItemText primary="Apply for Financing" />
         </ListItem>
-        <ListItem button component={Link} to="/financing-options" disabled={!isUserTokenValid}>
+        <ListItem button component={Link} to="/financing-options" disabled={!(isUserTokenValid==="")}>
           <ListItemIcon sx={{ color: 'white' }}>
             <AccountBalance />
           </ListItemIcon>
