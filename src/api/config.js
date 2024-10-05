0a5bcef875bfe2252ef875a74a2108eaf001862b
Anurag Kumar <AnKumar@flexera.com>
2024-10-05 14:39:54 +0530

    removed config.js

--- src/api/config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-    authService: process.env.REACT_APP_AUTH_SERVICE_URL,
-    financeService: process.env.REACT_APP_FINANCE_SERVICE_URL,
-    notificationService: process.env.REACT_APP_NOTIFICATION_SERVICE_URL,
-    analyticsService: process.env.REACT_APP_ANALYTICS_SERVICE_URL,
-  };
-  
-  export default config;
-  
