b4e5b4ee3243ec1cad010f025a2594f523177e6c
Anurag Kumar <AnKumar@flexera.com>
2024-10-13 00:53:26 +0530

    green deployment

--- src/api/apiService.js
+++ src/api/apiService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Base URL from environment or fallback to localhost
 // const baseURL = "https://beta.appsxyzabc.com" ; for green development
-const baseURL = "https://appsxyzabc.com" ; // for blue dev : todo: pass from environment
+const baseURL = "https://appsxyzabc.com"; // for blue dev : todo: pass from environment, issue with browser cache
 // Create Axios instances for different services
 const authApi = axios.create({ baseURL });
 const financeApi = axios.create({ baseURL });
